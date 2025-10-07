276a6f6a1c984ee7f1c88688e6909d59cc89b8ed
Merijn <560248@student.fontys.nl>
2025-10-07 14:05:40 +0200

    Adds Maven Wrapper and initial project setup
    Adds the Maven Wrapper to the project, enabling consistent builds across different environments.
    
    Sets up a basic Spring Boot application with a sample test and configuration file. Also configures `.gitignore` and `.gitattributes` files for the project.

---
+++ chefbackend/src/main/java/com/example/chefbackend/ChefbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.chefbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChefbackendApplication.class, args);
+    }
+
+}
---
+++ chefbackend/src/test/java/com/example/chefbackend/ChefbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chefbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
